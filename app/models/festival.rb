b7e8732a63bc70057a377765f96146108b18b0f7
Heidi Poon <heidixpoon@gmail.com>
2015-07-20 14:55:58 -0500

    Created Models

---
+++ app/models/festival.rb
@@ -0,0 +1,7 @@
+class Festival < ActiveRecord::Base
+
+  has_many :reviews
+  has_many :users, through: :reviews
+  
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 5 }
+  validates :password, confirmation: true
+  validates :email, uniqueness: true, email_format: { message: 'has invalid format' }
+
+  has_many :reviews
+  has_many :festivals, through: :reviews
+
+
 end
