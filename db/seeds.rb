0068ce8bf58595d5d33645f3e556115fde195679
Heidi Poon <heidixpoon@gmail.com>
2015-07-21 16:08:37 -0500

    Seeded Festivals & Put them on festival#index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,68 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+
+# FESTIVALS
+ultra = Festival.create([{
+    :event => 'Ultra Europe',
+    :date => 'July 10~12, 2015',
+    :address => 'Split, Croatia',
+    :pic => 'http://croatia-wave.com/wp-content/uploads/2015/01/lg-banner6.jpg' } ])
+
+acl = Festival.create([{
+    :event => 'Austin City Limits',
+    :date => 'October 2~4, 2015',
+    :address => 'Austin, TX',
+    :pic => 'https://randallmetting.files.wordpress.com/2012/07/acl-panoramic.jpg' } ])
+
+edc = Festival.create([{
+    :event => 'Electric Daisy Carnival',
+    :date => 'June 19~21, 2015',
+    :address => 'Las Vegas, NV',
+    :pic => 'http://www.youredm.com/wp-content/uploads/2015/06/edc.jpg' } ])
+
+tomorrowland = Festival.create([{
+    :event => 'Tomorrowland',
+    :date => 'July 24~26, 2015',
+    :address => 'Boom, Belgium',
+    :pic => 'http://www.youredm.com/wp-content/uploads/2015/02/tl-fest.jpg' } ])
+
+coachella = Festival.create([{
+    :event => 'Coachella',
+    :date => 'April 17~19, 2015',
+    :address => 'Indio, CA',
+    :pic => 'http://images.8tracks.com/cover/i/008/740/832/6355643126543580751243812664_coachella-2014.-1955.jpg?q=98&fm=jpg&fit=max' } ])
+
+forest = Festival.create([{
+    :event => 'Electric Forest',
+    :date => 'June 25~27, 2015',
+    :address => 'Rothbury, Michigan',
+    :pic => 'http://myvipeeps.com/wp-content/uploads/2013/07/1077869_546858528696189_563686781_o.jpg' } ])
+
+
+
+
+
+# REVIEWS
 #
-# Examples:
+# mag01 = Review.create([{
+#     :critique => "Before they sold out butcher selfies, meggings cardigan Tumblr roof party. PBR&B gentrify Godard, selvage pickled try-hard Odd Future aesthetic. "
+#     } ])
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# ale01 = Review.create([{
+#     :critique => "Vinyl brunch meh, dreamcatcher leggings bicycle rights flexitarian drinking vinegar tousled. "
+#     } ])
+#
+# shouse01 = Review.create([{
+#     :critique => "Master cleanse cliche roof party, Pitchfork blog you probably haven't heard of them Neutra. Vice leggings plaid, cardigan meh disrupt actually Echo Park food truck farm-to-table Wes Anderson. "
+#     } ])
+#
+#
+# olivia01 = Review.create([{
+#     :critique => "Kitsch pop-up keffiyeh Williamsburg, artisan fanny pack master cleanse authentic fashion axe mixtape vinyl scenester. "
+#     } ])
+#
+# pizza01 = Review.create([{
+#     :critique => "I'm over it. "
+#     } ])
+#
+# kerby01 = Review.create([{
+#     :critique => "Narwhal disrupt before they sold out PBR&B mustache distillery. Cornhole fanny pack fap viral. "
+#     } ])
